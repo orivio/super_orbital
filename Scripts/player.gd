2088e7283a587652fe9e35aa4bf8eadac48cca29
Obed Otto <57687329+ObedH@users.noreply.github.com>
2026-05-22 20:44:39 -0400

    Added literally the worst black holes imaginable

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -3,8 +3,11 @@ extends CharacterBody2D
 
 const SPEED = 700.0
 const JUMP_VELOCITY = -800.0
+const GRAVITY = 100000000
 
 var gravity = true
+
+@onready var black_holes = $"../BlackHoles"
 
 
 func _physics_process(delta: float) -> void:
@@ -28,6 +31,11 @@ func _physics_process(delta: float) -> void:
 		move_and_slide()
 	else:
 		var y = 0;
+		for ch in black_holes.get_children():
+			var distance = ch.position.distance_to(position);
+			var direction = (ch.position - position);
+			velocity += GRAVITY * delta * direction.normalized() / (distance * distance)
+			print(velocity)
 		move_and_slide()
 	
 	if Input.is_action_just_pressed("Gravity Switch"):
