708c152a79bcec9b53e66cb3bd8402080703b58e
orivio <russlanhossain@gmail.com>
2026-06-08 13:24:35 -0400

    yoooo i added some cool stuff!
    you can now bounce off of walls while in antigrav and i also started the wrench system

--- entities/player/state/state_float.gd
+++ entities/player/state/state_float.gd
@@ -1,13 +1,17 @@
 class_name StateFloat extends PlayerState
 
+var entry_velocity = 0
+
 @onready var idle_state: PlayerState = $"../Idle"
 @onready var walk_state: PlayerState = $"../Walk"
 @onready var fall_state: PlayerState = $"../Fall"
 @onready var jump_state: PlayerState = $"../Jump"
+@onready var wrench_state: PlayerState = $"../Wrench"
 
 func enter() -> void:
 	player.update_animation("float")
 	player.has_gravity = false
+	entry_velocity = player.velocity
 
 func exit() -> void:
 	pass
@@ -18,19 +22,35 @@ func process(_delta: float) -> PlayerState:
 func physics_process(delta: float) -> PlayerState:
 	
 	if player.is_on_floor():
-		if player.direction == 0:
-			return idle_state
-		else:
-			return walk_state
+		player.velocity.y = entry_velocity.y * -1
+		entry_velocity.y = player.velocity.y
+	#	if player.direction == 0:
+	#		return idle_state
+	#	else:
+	#		return walk_state
+	
+	if player.is_on_ceiling():
+		player.velocity.y = entry_velocity.y * -1
+		entry_velocity.y = player.velocity.y
 	
 	if player.is_on_wall():
-		return fall_state
+		player.velocity.x = entry_velocity.x * -1
+		entry_velocity.x = player.velocity.x
 	
 	if Input.is_action_just_pressed("gravity_switch"):
 		if player.velocity.y > 0:
 			return fall_state
 		else:
 			return jump_state
+	
+	if Input.is_action_just_pressed("throw_wrench"): #WIPWIPWIPWIPWIPWIP
+		var x_wrench = Input.get_axis("wrench_left", "wrench_right")
+		var y_wrench = Input.get_axis("wrench_up", "wrench_down")
+		var wrench_direction = Vector2(x_wrench, y_wrench) * -1
+		if wrench_direction != Vector2.ZERO:
+			player.velocity = wrench_direction
+		return wrench_state
+		#E = mc^2 - Rayyan Khan
 	
 	for black_hole: BlackHole in PhysicsManager.black_holes:
 		var direction: Vector2 = black_hole.global_position - player.global_position
---
+++ entities/player/state/state_wrench.gd
@@ -0,0 +1,40 @@
+class_name StateWrench extends PlayerState
+
+var entry_velocity = 0
+
+@onready var idle_state: PlayerState = $"../Idle"
+@onready var walk_state: PlayerState = $"../Walk"
+@onready var fall_state: PlayerState = $"../Fall"
+@onready var jump_state: PlayerState = $"../Jump"
+@onready var float_state: PlayerState = $"../Float"
+
+func enter() -> void:
+	player.update_animation("wrench")
+	entry_velocity = player.velocity
+	if not player.is_on_floor():
+		player.velocity = entry_velocity * -1
+
+func exit() -> void:
+	pass
+
+func process(_delta: float) -> PlayerState:
+	return null
+
+func physics_process(delta: float) -> PlayerState:
+	
+	if player.is_on_floor():
+		player.velocity.y = entry_velocity.y * -1
+		entry_velocity.y = player.velocity.y
+		#return float_state
+	
+	if player.is_on_ceiling():
+		player.velocity.y = entry_velocity.y * -1
+		entry_velocity.y = player.velocity.y
+		#return float_state
+	
+	if player.is_on_wall():
+		player.velocity.x = entry_velocity.x * -1
+		entry_velocity.x = player.velocity.x
+		#return float_state
+	
+	return null
