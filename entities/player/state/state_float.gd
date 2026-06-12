825e2a123da785af8918a9f16c38c520bc487cb0
Obed Otto <57687329+ObedH@users.noreply.github.com>
2026-06-12 13:20:15 -0400

    Refactor player state to use base_velocity

--- entities/player/state/state_float.gd
+++ entities/player/state/state_float.gd
@@ -11,7 +11,7 @@ var entry_velocity = 0
 func enter() -> void:
 	player.update_animation("float")
 	player.has_gravity = false
-	entry_velocity = player.velocity
+	entry_velocity = player.base_velocity
 
 func exit() -> void:
 	pass
@@ -22,27 +22,27 @@ func process(_delta: float) -> PlayerState:
 func physics_process(delta: float) -> PlayerState:
 	
 	if player.is_on_floor():
-		player.velocity.y = entry_velocity.y * -1 * game_manager.time_scale
-		entry_velocity.y = player.velocity.y
+		player.base_velocity.y = entry_velocity.y * -1
+		entry_velocity.y = player.base_velocity.y
 	#	if player.direction == 0:
 	#		return idle_state
 	#	else:
 	#		return walk_state
 	
 	if player.is_on_ceiling():
-		player.velocity.y = entry_velocity.y * -1
-		entry_velocity.y = player.velocity.y
+		player.base_velocity.y = entry_velocity.y * -1
+		entry_velocity.y = player.base_velocity.y
 	
 	if player.is_on_wall():
-		player.velocity.x = entry_velocity.x * -1
-		entry_velocity.x = player.velocity.x
+		player.base_velocity.x = entry_velocity.x * -1
+		entry_velocity.x = player.base_velocity.x
 		player.sprite.flip_h = not player.sprite.flip_h
 	
 	if Input.is_action_just_pressed("gravity_switch"):
 		player.has_gravity = true
 	
 	if player.has_gravity:
-		if player.velocity.y > 0:
+		if player.base_velocity.y > 0:
 			return fall_state
 		else:
 			return jump_state
@@ -52,10 +52,10 @@ func physics_process(delta: float) -> PlayerState:
 		var y_wrench = Input.get_axis("wrench_up", "wrench_down")
 		var wrench_direction = Vector2(x_wrench, y_wrench)
 		if wrench_direction != Vector2.ZERO:
-			player.velocity = wrench_direction.normalized() * player.movement_settings.wrench_velocity * game_manager.time_scale
-			if player.velocity.x > 0:
+			player.base_velocity = wrench_direction.normalized() * player.movement_settings.wrench_velocity
+			if player.base_velocity.x > 0:
 				player.sprite.flip_h = false
-			elif player.velocity.x < 0:
+			elif player.base_velocity.x < 0:
 				player.sprite.flip_h = true
 		return wrench_state
 		#E = mc^2 - Rayyan Khan
@@ -65,5 +65,5 @@ func physics_process(delta: float) -> PlayerState:
 		var distance: float = direction.length_squared()
 		var velocity: Vector2 = PhysicsManager.GRAVITY_CONSTANT * (direction.normalized() / distance) * black_hole.mass / player.movement_settings.mass
 		
-		player.velocity += velocity * game_manager.time_scale
+		player.base_velocity += velocity
 	return null
