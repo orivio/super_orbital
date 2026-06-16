ec84f82d2968c28886b7671e50289da37ac16eda
Obed Otto <57687329+ObedH@users.noreply.github.com>
2026-06-16 15:50:46 -0400

    a

--- shared/globals/game_manager.gd
+++ shared/globals/game_manager.gd
@@ -3,11 +3,16 @@ extends Node
 var player: Player
 var camera: PlayerCamera
 var time_scale: float = 1
-var timer: float = 0
+var impact_timer: float = 0
+
 func impact():
-	time_scale = 0.1
-	timer = 0.1
+	if impact_timer <= 0 and time_scale == 1:
+		time_scale = 0.0001
+		impact_timer = 0.1
+
 func _physics_process(delta: float) -> void:
-	timer -= delta
-	if timer < 0:
-		time_scale = 1
+	if time_scale != 1:
+		impact_timer -= delta
+		if impact_timer < 0:
+			camera.camera_shake.emit()
+			time_scale = 1
--- systems/camera/player_camera.gd
+++ systems/camera/player_camera.gd
@@ -1,15 +1,30 @@
 class_name PlayerCamera extends Camera2D
 
+signal camera_shake
+
+@export var shake_fade: float = 1
+@export var random_strength: float = 100
+
 var target: Vector2
+var rng: RandomNumberGenerator = RandomNumberGenerator.new()
+var shake_strength: float = 0.0
+var thing: float = 0
 
 func _ready() -> void:
 	GameManager.camera = self
+	camera_shake.connect(on_camera_shake)
 
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
+	
+	var shake_offset: Vector2 = Vector2.ZERO
+	
+	if shake_strength > 0:
+		#shake_strength = lerpf(shake_strength, 0, shake_fade * delta)
+		shake_offset = random_offset()
 	
 	target = GameManager.player.global_position
-	
-	position = target
+	thing += delta
+	position = target + Vector2.UP.rotated(thing)
 
 func set_limits(rect: Rect2) -> void:
 	limit_left = rect.position.x - rect.size.x / 2
@@ -18,3 +33,9 @@ func set_limits(rect: Rect2) -> void:
 	limit_top = rect.position.y - rect.size.y / 2
 	
 	# print("Setting limits: (left: ", limit_left, ", right: ", limit_right, ", top: ", limit_top, ", bottom: ", limit_bottom, ")")
+
+func on_camera_shake() -> void:
+	shake_strength = random_strength
+
+func random_offset() -> Vector2:
+	return Vector2(rng.randf_range(-shake_strength, shake_strength), rng.randf_range(-shake_strength,shake_strength))
