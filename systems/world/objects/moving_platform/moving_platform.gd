2996cac993451fba157ec1ee1ebb163241b1877f
Obed Otto <57687329+ObedH@users.noreply.github.com>
2026-06-19 17:43:57 -0400

    Added moving platforms and a level that teaches you the mechanic

---
+++ systems/world/objects/moving_platform/moving_platform.gd
@@ -0,0 +1,14 @@
+extends Path2D
+
+@export var speed: float = 1
+@export var forward_direction: float = 1
+
+@onready var path_follow_2d: PathFollow2D = $PathFollow2D
+
+func _physics_process(delta: float) -> void:
+	path_follow_2d.progress_ratio += speed * delta * forward_direction
+	
+	if forward_direction == 1 and path_follow_2d.progress_ratio == 1:
+		forward_direction = -1
+	elif forward_direction == -1 and path_follow_2d.progress_ratio == 0:
+		forward_direction = 1
