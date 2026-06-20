3f9a56631b0679155f8e855dd46790356ebf4156
Obed Otto <57687329+ObedH@users.noreply.github.com>
2026-06-20 14:03:24 -0400

    Adding another platform level, still WIP

--- systems/world/objects/moving_platform/moving_platform.gd
+++ systems/world/objects/moving_platform/moving_platform.gd
@@ -1,9 +1,14 @@
 extends Path2D
 
-@export var speed: float = 1
+@export var time: float = 1
 @export var forward_direction: float = 1
 
+var speed: float = 1
+
 @onready var path_follow_2d: PathFollow2D = $PathFollow2D
+
+func _ready() -> void:
+	speed = 1 / time
 
 func _physics_process(delta: float) -> void:
 	path_follow_2d.progress_ratio += speed * delta * forward_direction
