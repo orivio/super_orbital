7df9bc55a329daf26daa3fc0b8f1bc77178b976c
Obed Otto <57687329+ObedH@users.noreply.github.com>
2026-06-25 08:18:38 -0400

    Added horizontally facing spikes (crazy concept) and tried to make a cooler background shader effect

--- systems/world/rooms/tutorial/jump/bridge_spikes.gd
+++ systems/world/rooms/tutorial/jump/bridge_spikes.gd
@@ -21,17 +21,47 @@ func spike_switch() -> void:
 	if !state_a:
 		tilemap.set_cell(Vector2i(25, -5), 0, Vector2i(1, 0))
 		tilemap.set_cell(Vector2i(26, -5), 0, Vector2i(1, 0))
+		tilemap.set_cell(Vector2i(25, -3), 0, Vector2i(2, 0))
+		tilemap.set_cell(Vector2i(26, -3), 0, Vector2i(2, 0))
+		tilemap.set_cell(Vector2i(24, -4), 0, Vector2i(3, 0))
+		tilemap.set_cell(Vector2i(27, -4), 0, Vector2i(4, 0))
+		
 		tilemap.set_cell(Vector2i(26, -11), 0, Vector2i(1, 0))
 		tilemap.set_cell(Vector2i(27, -11), 0, Vector2i(1, 0))
+		tilemap.set_cell(Vector2i(26, -9), 0, Vector2i(2, 0))
+		tilemap.set_cell(Vector2i(27, -9), 0, Vector2i(2, 0))
+		tilemap.set_cell(Vector2i(25, -10), 0, Vector2i(3, 0))
+		tilemap.set_cell(Vector2i(28, -10), 0, Vector2i(4, 0))
+		
+		
 		
 		tilemap.set_cell(Vector2i(21, -8), -1)
 		tilemap.set_cell(Vector2i(22, -8), -1)
+		tilemap.set_cell(Vector2i(21, -6), -1)
+		tilemap.set_cell(Vector2i(22, -6), -1)
+		tilemap.set_cell(Vector2i(23, -7), -1)
+		tilemap.set_cell(Vector2i(20, -7), -1)
 	else:
 		tilemap.set_cell(Vector2i(25, -5), -1)
 		tilemap.set_cell(Vector2i(26, -5), -1)
+		tilemap.set_cell(Vector2i(25, -3), -1)
+		tilemap.set_cell(Vector2i(26, -3), -1)
+		tilemap.set_cell(Vector2i(24, -4), -1)
+		tilemap.set_cell(Vector2i(27, -4), -1)
+		
 		tilemap.set_cell(Vector2i(26, -11), -1)
 		tilemap.set_cell(Vector2i(27, -11), -1)
+		tilemap.set_cell(Vector2i(26, -9), -1)
+		tilemap.set_cell(Vector2i(27, -9), -1)
+		tilemap.set_cell(Vector2i(25, -10), -1)
+		tilemap.set_cell(Vector2i(28, -10), -1)
+		
+		
 		
 		tilemap.set_cell(Vector2i(21, -8), 0, Vector2i(1, 0))
 		tilemap.set_cell(Vector2i(22, -8), 0, Vector2i(1, 0))
+		tilemap.set_cell(Vector2i(21, -6), 0, Vector2i(2, 0))
+		tilemap.set_cell(Vector2i(22, -6), 0, Vector2i(2, 0))
+		tilemap.set_cell(Vector2i(20, -7), 0, Vector2i(3, 0))
+		tilemap.set_cell(Vector2i(23, -7), 0, Vector2i(4, 0))
 	state_a = not state_a
