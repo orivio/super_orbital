c2cacf66176b413a4597cd3c26d055c50feb150d
Russlan Hossain <84582272+orivio@users.noreply.github.com>
2026-06-27 23:59:24 -0400

    added a wrench item that gives wrench throw back midair
    check if my script works please at wrench_item.gd

---
+++ systems/world/objects/wrench_item/wrench_item.gd
@@ -0,0 +1,10 @@
+# please check if this works lol - russ
+class_name WrenchItem extends Area2D
+
+func on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		wrench_get(body)
+
+func wrench_get(player: Node2D):
+	player.can_throw_wrench = true
+	queue_free()
