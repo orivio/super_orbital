a160a8b448fa64ebce098a83a783c53ec4688f91
Obed Otto <57687329+ObedH@users.noreply.github.com>
2026-07-12 16:32:43 -0400

    Made new tiles and applied them, changed moving platform textures, and adjusted some level's difficulty

--- objects/moving_platform/moving_platform.gd
+++ objects/moving_platform/moving_platform.gd
@@ -9,7 +9,7 @@ var rotate_speed: float = 0
 
 @onready var path_follow_2d: PathFollow2D = $PathFollow2D
 @onready var collision_shape: CollisionShape2D = $AnimatableBody2D/CollisionShape2D
-@onready var color_rect: ColorRect = $AnimatableBody2D/ColorRect
+@onready var rect: NinePatchRect = $AnimatableBody2D/NinePatchRect
 
 func _ready() -> void:
 	speed = 1 / time
@@ -20,7 +20,7 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	if rotate_time != 0:
 		collision_shape.rotation += rotate_speed * delta * GameManager.time_scale
-		color_rect.rotation += rotate_speed * delta * GameManager.time_scale
+		rect.rotation += rotate_speed * delta * GameManager.time_scale
 	
 	path_follow_2d.progress_ratio += speed * delta * GameManager.time_scale * forward_direction
 	
