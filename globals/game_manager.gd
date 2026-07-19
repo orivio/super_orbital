643e1a2d8e143d1b4e7c7dbdb5a56184e77c2dd0
Russlan Hossain <84582272+orivio@users.noreply.github.com>
2026-07-19 00:43:19 -0400

    tried to make entry on black hole change timescale
    it does but only for like frame, idk how to stop that but i could figure it out at 1pm inst4ead of 1am maybe if i lock in

--- globals/game_manager.gd
+++ globals/game_manager.gd
@@ -5,11 +5,13 @@ var camera: PlayerCamera
 var current_room: Room
 var time_scale: float = 1
 var impact_timer: float = 0
+var blackhole: BlackHole #NEED TO MAKE IT SO THAT THE VALUE OF TIME_SCALE DOES NOT RESET WHILE IN BVLACK HOLE ORBIT
 
 func impact():
 	if impact_timer <= 0 and time_scale == 1:
 		time_scale = 0.0001
 		impact_timer = 0.1
+
 
 func _physics_process(delta: float) -> void:
 	if time_scale != 1:
