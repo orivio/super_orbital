660f68266bd82c97a15216c4ed3194486bdeea06
Obed Otto <57687329+ObedH@users.noreply.github.com>
2026-07-26 12:08:52 -0400

    Added audio settings to the menu

---
+++ scenes/settings/settings_audio.gd
@@ -0,0 +1,49 @@
+extends Control
+
+@onready var master_volume: HSlider = $VBoxContainer/GridContainer/HSlider
+@onready var music_volume: HSlider = $VBoxContainer/GridContainer/HSlider2
+@onready var sound_effects_volume: HSlider = $VBoxContainer/GridContainer/HSlider3
+@onready var audio_devices: OptionButton = $VBoxContainer/GridContainer/OptionButton
+@onready var refresh_audio_devices: Button = $VBoxContainer/GridContainer/Button
+@onready var refresh_aduio_device_list_timer: Timer = $Timer
+
+var audio_device_list: Array[StringName] = []
+var audio_device_list_refresh_timer: Timer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var idx: int = 0
+	audio_device_list.clear()
+	for device in AudioServer.get_output_device_list():
+		audio_devices.add_item(device)
+		audio_device_list.append(device)
+		if device == AudioServer.output_device:
+			audio_devices.selected = idx
+		
+		idx += 1
+	
+	refresh_aduio_device_list_timer.start()
+	
+
+func _on_master_volume_value_changed(value: float) -> void:
+	var master_idx: int = AudioServer.get_bus_index(&"Master")
+	AudioServer.set_bus_volume_db(master_idx, remap(value, 0, 100, -60, 0))
+
+
+func _on_music_volume_value_changed(value: float) -> void:
+	var music_idx: int = AudioServer.get_bus_index(&"Music")
+	AudioServer.set_bus_volume_db(music_idx, remap(value, 0, 100, -60, 0))
+
+
+func _on_sound_effect_volume_slider_value_changed(value: float) -> void:
+	var sfx_idx: int = AudioServer.get_bus_index(&"Sound Effects")
+	AudioServer.set_bus_volume_db(sfx_idx, remap(value, 0, 100, -60, 0))
+
+
+func _on_audio_device_item_selected(index: int) -> void:
+	AudioServer.output_device = audio_device_list[index]
+
+func _on_refresh_audio_device_list() -> void:
+	print("Refreshing audio device list")
+	refresh_aduio_device_list_timer.start()
