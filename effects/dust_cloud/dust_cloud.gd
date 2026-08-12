13bbcbc52c75bdd3f2bfe95495770d7bbdf5f915
Obed Otto <57687329+ObedH@users.noreply.github.com>
2026-08-12 11:29:07 -0400

    Did pretty much nothing

--- effects/dust_cloud/dust_cloud.gd
+++ effects/dust_cloud/dust_cloud.gd
@@ -5,16 +5,11 @@ extends CPUParticles2D
 @onready var timer: Timer = $Timer
 
 func _ready() -> void:
-	print("READY: ", global_position)
 	emitting = false
 
 func start() -> void:
-	print("START: ", global_position)
 	emitting = true
 	timer.start(emission_time)
 
 func _on_timer_timeout() -> void:
 	emitting = false
-
-func _physics_process(delta: float) -> void:
-	print("UPDATE: ", global_position)
