9beb66ace2a4e347231e5e7885180a74faf62aae
Obed Otto <57687329+ObedH@users.noreply.github.com>
2026-08-29 19:05:32 -0400

    Added the basic idea of level selecting in game

--- scenes/pause_menu/pause_menu.gd
+++ scenes/pause_menu/pause_menu.gd
@@ -4,9 +4,42 @@ extends Control
 signal close_pressed
 signal exit_pressed
 
+var pause_options: PauseOptions = null
+var level_grid: LevelGrid = null
+
+@onready var panel_container: PanelContainer = $PanelContainer
+
+
+func _ready() -> void:
+	spawn_pause_options()
+
+
+func spawn_pause_options() -> void:
+	var pause_options_scene: PackedScene = load("res://scenes/pause_menu/pause_options.tscn")
+	pause_options = pause_options_scene.instantiate()
+	pause_options.close_pressed.connect(_on_resume_button_pressed)
+	pause_options.level_select_pressed.connect(_on_level_select_button_pressed)
+	pause_options.exit_pressed.connect(_on_exit_button_pressed)
+	panel_container.add_child(pause_options)
+
+
+func spawn_level_grid() -> void:
+	var level_grid_scene: PackedScene = load("res://scenes/save_select/level_grid.tscn")
+	level_grid = level_grid_scene.instantiate()
+	panel_container.add_child(level_grid)
+	if not level_grid.is_node_ready():
+		await level_grid.ready
+	level_grid.update_visuals(SaveManager.get_save_file())
+	panel_container.queue_sort()
+
 
 func _on_resume_button_pressed() -> void:
 	close_pressed.emit()
+
+
+func _on_level_select_button_pressed() -> void:
+	pause_options.queue_free()
+	spawn_level_grid()
 
 
 func _on_exit_button_pressed() -> void:
---
+++ scenes/pause_menu/pause_options.gd
@@ -0,0 +1,18 @@
+class_name PauseOptions
+extends MarginContainer
+
+signal close_pressed
+signal level_select_pressed
+signal exit_pressed
+
+
+func _on_resume_button_pressed() -> void:
+	close_pressed.emit()
+
+
+func _on_level_select_button_pressed() -> void:
+	level_select_pressed.emit()
+
+
+func _on_exit_button_pressed() -> void:
+	exit_pressed.emit()
