c0dac58f65771bc3870ee6e483713cb175de8aa1
Obed Otto <57687329+ObedH@users.noreply.github.com>
2026-08-31 17:02:16 -0400

    Added dash to the player movement settings

--- player/movement_settings/player_movement_settings.gd
+++ player/movement_settings/player_movement_settings.gd
@@ -23,14 +23,14 @@ extends Resource
 			air_acceleration = walk_speed / air_walk_start_time
 		else:
 			air_acceleration = max_acceleration
-@export_range(0, 1) var walk_start_time: float:
+@export_range(0.01, 1) var walk_start_time: float:
 	set(value):
 		walk_start_time = value
 		if walk_start_time != 0:
 			ground_acceleration = walk_speed / walk_start_time
 		else:
 			ground_acceleration = max_acceleration
-@export_range(0, 1) var walk_stop_time: float:
+@export_range(0.01, 1) var walk_stop_time: float:
 	set(value):
 		walk_stop_time = value
 		if walk_stop_time != 0:
@@ -53,27 +53,38 @@ extends Resource
 		jump_fall_time = value
 		fall_gravity_acceleration = 2 * jump_max_height / (jump_fall_time * jump_fall_time)
 @export_category("Air Settings")
-@export var air_walk_start_time: float:
+@export_range(0.01, 9.0) var air_walk_start_time: float:
 	set(value):
 		air_walk_start_time = value
 		if air_walk_start_time != 0:
 			air_acceleration = walk_speed / air_walk_start_time
 		else:
 			air_acceleration = max_acceleration
-@export var air_walk_stop_time: float:
+@export_range(0.01, 9.0) var air_walk_stop_time: float:
 	set(value):
 		air_walk_stop_time = value
 		if air_walk_stop_time != 0:
 			air_friction = walk_speed / air_walk_stop_time
 		else:
 			air_friction = max_acceleration
+@export_category("Dash Settings")
+@export_range(10, 512) var dash_distance: float:
+	set(value):
+		dash_distance = value
+		if dash_time != 0:
+			dash_speed = dash_distance / dash_time
+@export_range(0.3, 2.0) var dash_time: float:
+	set(value):
+		dash_time = value
+		if dash_time != 0:
+			dash_speed = dash_distance / dash_time
 @export_category("Juice")
 @export_range(0, 1.0) var jump_buffer_time: float
 @export_range(0, 2.0) var coyote_time: float
 @export_category("Limits")
 @export_range(0, 1000) var minimum_movement_threshold: float
 @export var max_acceleration: float = 999999
-@export var ceiling_clip_min_velocity: float
+@export_range(32.0, 1200.0) var ceiling_clip_min_velocity: float
 @export_category("Hidden Settings")
 @export var ground_friction: float
 @export var ground_acceleration: float
@@ -83,3 +94,4 @@ extends Resource
 @export var normal_gravity_acceleration: float
 @export var peak_gravity_acceleration: float
 @export var fall_gravity_acceleration: float
+@export var dash_speed: float
