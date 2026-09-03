de327412ee154c16ce8678418dbf7a0e2deaec4d
Obed Otto <57687329+ObedH@users.noreply.github.com>
2026-09-03 14:13:28 -0400

    Added button animations to the menu

--- ui/effects/button_animation/button_animator.gd
+++ ui/effects/button_animation/button_animator.gd
@@ -1,11 +1,31 @@
+class_name ButtonAnimator
 extends Button
 
+@export var ease_type: Tween.EaseType
+@export var trans_type: Tween.TransitionType
+@export var anim_duration: float
+@export var scale_amount: float
 
-# Called when the node enters the scene tree for the first time.
+var tween: Tween
+
+
 func _ready() -> void:
-	pass # Replace with function body.
+	offset_transform_enabled = true
+	mouse_entered.connect(_on_mouse_entered)
+	mouse_exited.connect(_on_mouse_exited)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func reset_tween() -> void:
+	if tween:
+		tween.kill()
+	tween = create_tween().set_ease(ease_type).set_trans(trans_type).set_parallel(true)
+
+
+func _on_mouse_entered() -> void:
+	reset_tween()
+	tween.tween_property(self, "offset_transform_scale", Vector2(scale_amount, scale_amount), anim_duration)
+
+
+func _on_mouse_exited() -> void:
+	reset_tween()
+	tween.tween_property(self, "offset_transform_scale", Vector2.ONE, anim_duration)
