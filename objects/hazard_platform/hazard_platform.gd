91fedad6b94cf5fea05df08c8d959d6126f3e76b
Obed Otto <57687329+ObedH@users.noreply.github.com>
2026-09-06 17:53:56 -0400

    Added moving saws and added them to the level "Facility Entrance"

--- objects/hazard_platform/hazard_platform.gd
+++ objects/hazard_platform/hazard_platform.gd
@@ -1,23 +1,21 @@
 @tool
-class_name MovingPlatform
+class_name HazardPlatform
 extends Path2D
 
 @export var time: float = 1
 @export var forward_direction: float = 1
-@export var rotate_time: float = 0
+@export var rotate_speed: float = 0
 @export_tool_button("Update Follow Visual") var update_line_button = update_follow_visual
 
 var speed: float = 1
-var rotate_speed: float = 0
 
 @onready var path_follow_2d: PathFollow2D = $PathFollow2D
 @onready var collision_shape: CollisionShape2D = $AnimatableBody2D/CollisionShape2D
+@onready var sprite_2d: Sprite2D = $AnimatableBody2D/Sprite2D
 @onready var follow_visual: Line2D = $FollowVisual
 
 func _ready() -> void:
 	speed = 1 / time
-	if rotate_time != 0:
-		rotate_speed = 2 * PI / rotate_time
 	
 	update_follow_visual()
 
@@ -27,6 +25,7 @@ func _physics_process(delta: float) -> void:
 			return
 		
 		path_follow_2d.progress_ratio += speed * delta * GameManager.time_scale * forward_direction
+		sprite_2d.rotation_degrees += rotate_speed * delta * GameManager.time_scale
 		
 		if forward_direction == 1 and path_follow_2d.progress_ratio == 1:
 			forward_direction = -1
