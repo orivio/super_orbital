eb0144ad2c38506e9e953cdcf618f7eaa39446a8
Obed Otto <57687329+ObedH@users.noreply.github.com>
2026-09-06 08:09:44 -0400

    Created "elevator" platforms

--- objects/moving_platform/lifter.gd
+++ objects/moving_platform/lifter.gd
@@ -1,24 +1,21 @@
 @tool
-class_name MovingPlatform
+class_name Lifter
 extends Path2D
 
 @export var time: float = 1
-@export var forward_direction: float = 1
-@export var rotate_time: float = 0
+@export var forward_direction: float = 0
 @export_tool_button("Update Follow Visual") var update_line_button = update_follow_visual
 
 var speed: float = 1
-var rotate_speed: float = 0
 
 @onready var path_follow_2d: PathFollow2D = $PathFollow2D
 @onready var collision_shape: CollisionShape2D = $AnimatableBody2D/CollisionShape2D
 @onready var rect: NinePatchRect = $AnimatableBody2D/NinePatchRect
 @onready var follow_visual: Line2D = $FollowVisual
+@onready var player_detector: Area2D = $AnimatableBody2D/Area2D
 
 func _ready() -> void:
 	speed = 1 / time
-	if rotate_time != 0:
-		rotate_speed = 2 * PI / rotate_time
 	
 	update_follow_visual()
 
@@ -26,16 +23,25 @@ func _physics_process(delta: float) -> void:
 	if not Engine.is_editor_hint():
 		if time == 0:
 			return
-		if rotate_time != 0:
-			collision_shape.rotation += rotate_speed * delta * GameManager.time_scale
-			rect.rotation += rotate_speed * delta * GameManager.time_scale
 		
+		if path_follow_2d.progress_ratio >= 1:
+			path_follow_2d.progress_ratio = 1
+			if forward_direction == 1:
+				return
+		if path_follow_2d.progress_ratio <= 0:
+			path_follow_2d.progress_ratio = 0
+			if forward_direction == -1:
+				return
 		path_follow_2d.progress_ratio += speed * delta * GameManager.time_scale * forward_direction
-		
-		if forward_direction == 1 and path_follow_2d.progress_ratio == 1:
-			forward_direction = -1
-		elif forward_direction == -1 and path_follow_2d.progress_ratio == 0:
-			forward_direction = 1
 
 func update_follow_visual() -> void:
 	follow_visual.points = curve.get_baked_points()
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body is Player:
+		forward_direction = 1
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	forward_direction = -1
