b1fb4893835306693ddd5e7c77b676e0289d4390
Obed Otto <57687329+ObedH@users.noreply.github.com>
2026-09-13 09:30:52 -0400

    Added cutscene skipping

--- cutscenes/cutscene.gd
+++ cutscenes/cutscene.gd
@@ -12,5 +12,11 @@ func start() -> void:
 	cutscene_director.play(animation_name)
 
 
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		cutscene_director.stop()
+		cutscene_finished.emit()
+
+
 func _on_cutscene_director_animation_finished(_anim_name: StringName) -> void:
 	cutscene_finished.emit()
