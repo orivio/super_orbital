581f4592d6b44d7795f82d473143a69dc66fe655
Obed Otto <57687329+ObedH@users.noreply.github.com>
2026-09-20 16:51:11 -0400

    Added the black hole death sound effect

--- player/hurt_box.gd
+++ player/hurt_box.gd
@@ -8,12 +8,8 @@ func _on_body_entered(body: Node2D) -> void:
 	if body is TileMapLayer or body is AnimatableBody2D:
 		player.take_hit()
 		player.play_spike_death_sound()
-	elif body.is_in_group("black_hole"):
-		player.take_hit()
-		player.play_black_hole_death_sound()
 
 
 func _on_area_entered(_area: Area2D) -> void:
-	#if area.is_in_group("black_hole") and not player.disabled:
-	#	player.die()
-	pass
+	player.take_hit()
+	player.play_black_hole_death_sound()
